c4089270ffb6228b5e49ccf61546e519ce381f53
Ayla122 <ayladd122@gmail.com>
2021-12-15 19:11:46 -0800

    added components file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hello hello
+          Ayla Moss
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          link
         </a>
       </header>
     </div>

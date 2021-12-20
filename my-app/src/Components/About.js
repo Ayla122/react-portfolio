fd6f85418474aa375d7aed7a862c8de87ce0b982
Ayla122 <ayladd122@gmail.com>
2021-12-19 17:09:09 -0800

    finished about

--- my-app/src/Components/About.js
+++ my-app/src/Components/About.js
@@ -3,7 +3,11 @@ import React from 'react';
 function About (){
     console.log("About component")
     return(
+        <div>
         <h1>About me</h1>
+        <h2> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ullamcorper sollicitudin massa id placerat. Aenean sollicitudin bibendum arcu, maximus pharetra elit condimentum nec.  </h2>
+        <h2> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ullamcorper sollicitudin massa id placerat. Aenean sollicitudin bibendum arcu, maximus pharetra elit condimentum nec. </h2>
+        </div>
     )
 }
 

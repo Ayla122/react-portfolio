6cda4a8472146d0be2d94e8366ffbdd3926e3db2
Ayla122 <ayladd122@gmail.com>
2021-12-19 17:52:20 -0800

    rewrite contact

--- my-app/src/Components/Contact.js
+++ my-app/src/Components/Contact.js
@@ -1,74 +1,59 @@
-// contact form with fields for
-// a name,
-// an email address, and
-// a message
-// WHEN I move my cursor out of one of the form fields without entering text THEN I receive a notification that this field is required
-// WHEN I enter text into the email address field THEN I receive a notification if I have entered an invalid email address
-
 import React, { useState } from 'react';
-
+import ReactDOM from 'react-dom';
 import { validateEmail } from '../utils/Helpers';
 
-function Contact() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+function ContactForm() {
 
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log('Submit Form', formState);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+    
+                if(!isValid) {
+                    setErrorMessage('please enter a valid email');
+                } else {
+                    setErrorMessage('');
+                }
+
+            } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                } 
+        }
+
+        if (!errorMessage) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+        }
     }
-  };
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`A ${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
+    function handleSubmit(e) {
+        e.preventDefault();
     }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
-    }
-  };
 
-  return (
+return (
     <div>
-      <p className="content is-medium">Contact Me</p>
-      <hr />
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div className="field">
-          <label className="label" htmlFor="name">Name</label>
-          <input className="input" type="text" name="name" defaultValue={name} onBlur={handleChange} />
-        </div>
-        <div className="field">
-          <label className="label" htmlFor="email">Email Address</label>
-          <input className="input" type="email" name="email" defaultValue={email} onBlur={handleChange} />
-        </div>
-        <div className="field">
-          <label className="label" htmlFor="message">Message</label>
-          <textarea className="textarea" name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="is-danger">{errorMessage}</p>
-          </div>
-        )}
-        <button className="button is-medium is-primary is-fullwidth" data-testid="button" type="submit">Submit</button>
-      </form>
+        <div className="form-group">
+      <label for="name">Name: </label>
+      <input type="name" className="form-control" id="name" />
     </div>
-  );
+    <div className="form-group">
+      <label for="email">Email address:</label>
+      <input type="email" className="form-control" id="email" />
+    </div>
+    <div className="form-group">
+      <label for="mess">Message</label>
+      <input type="message" className="form-control" id="mess" />
+    </div>
+    <button type="submit" className="btn btn-default">Login</button>
+ </div>
+    );
 }
-
-export default Contact;
+    
+export default ContactForm;

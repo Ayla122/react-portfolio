a1d3375427c82d35215b30857bdae540fc6b340c
Ayla122 <ayladd122@gmail.com>
2021-12-19 18:09:08 -0800

    add resume

--- my-app/src/Components/Resume.js
+++ my-app/src/Components/Resume.js
@@ -1,9 +1,43 @@
-import React from 'react'; 
+import React from "react";
 
-function Resume(){
-    return(
-        <h1>Resume me</h1>
-    )
+function Resume() {
+  return (
+    <section class="container">
+      <h1>Resume</h1>
+
+      <div>
+        <div class="mt-5">
+
+          <p class="mt-5">
+            <a href="">
+              <img
+                src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+                alt="inkedIn"
+              />
+            </a>
+          </p>
+
+          <a
+            href=""
+            class="link"
+          >
+            Download Resume
+          </a>
+        </div>
+      </div>
+
+      <div class="justify-content-center mt-5">
+        <div>
+          <h2 className="top-title"></h2>
+          <ul>
+            <li>
+           
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Resume; 
+export default Resume;

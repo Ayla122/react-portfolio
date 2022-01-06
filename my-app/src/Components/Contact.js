feaeeea4449c3cabd8388c497424e5b26405db49
Ayla122 <ayladd122@gmail.com>
2022-01-06 15:49:59 -0800

    updated contact info

--- my-app/src/Components/Contact.js
+++ my-app/src/Components/Contact.js
@@ -8,30 +8,30 @@ function ContactForm() {
     return (
         <section>
             <form id="contact-form">
-                <h1>Contact me</h1>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
-                </div>
-                <div>
-                    <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-            <p class="mt-5">
+                <h1 id="contactMe">Contact me</h1>
+            
                 <a href="https://www.linkedin.com/in/ayla-moss-260967211/">
                     <img
                         src="https://img.icons8.com/fluent/48/000000/linkedin.png"
                         alt="inkedIn"
                     />
                 </a>
-            </p>
            
+                <a href="mailto:devaylamoss@gmail.com">
+                    <img id="gmail"
+                        src="https://upload.wikimedia.org/wikipedia/commons/8/8c/Gmail_Icon_%282013-2020%29.svg"
+                        alt="gmail"
+                    />
+                </a>
+
+                <a href="tel:5035198565">
+                <img id="phone"
+                        src="https://upload.wikimedia.org/wikipedia/commons/6/63/Black_telephone_icon_from_DejaVu_Sans.svg"
+                        alt="phone"
+                    />
+                </a>
+            
+           </form>
         </section>
     )
 }
